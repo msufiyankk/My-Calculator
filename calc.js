13c3220934b27ef41c3e7a78e4dfdb087e27bd87
msufiyankk <soofiyow@gmail.com>
2025-06-17 14:37:06 +0530

    [Init] Initialized Calculation

--- calc.js
+++ calc.js
@@ -1,1 +1,20 @@
-console.log("hr");
+console.log("Helo");
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = '';
+}
+function backDisplay(){
+    display.value = display.value.slice(0, -1);
+}
+function displaySum(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch{
+        display.value = "Synthax Error"
+    }
+}
